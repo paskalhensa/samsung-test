d695bcfd4e97365a238ef31a64087a2c77f7cd30
paskalhensa <paskalis_henry@yahoo.com>
2025-06-01 01:10:42 +0700

    feat: catch exception to give proper response payload in CreateDeviceHandler and refactor the saving process to add save to device target countries table

--- src/main/java/org/example/handlers/CreateDeviceHandler.java
+++ src/main/java/org/example/handlers/CreateDeviceHandler.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.dtos.CreateDeviceDto;
 import org.example.dtos.ResponseDto;
 import org.example.services.DeviceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ratpack.core.handling.Context;
 import ratpack.core.handling.Handler;
 
 import javax.inject.Inject;
+import java.sql.SQLException;
 
 public class CreateDeviceHandler implements Handler {
+    private static final Logger log = LoggerFactory.getLogger(CreateDeviceHandler.class);
     private final DeviceService deviceService;
 
     @Inject
@@ -20,8 +24,13 @@ public class CreateDeviceHandler implements Handler {
     @Override
     public void handle(Context ctx) {
         ctx.parse(CreateDeviceDto.class).then(device -> {
-            deviceService.createDevice(device);
-            ctx.getResponse().status(201).send(new ObjectMapper().writeValueAsString(new ResponseDto(true, "Device succesfully created", null, null)));
+            try {
+                deviceService.createDevice(device);
+                ctx.getResponse().status(201).send(new ObjectMapper().writeValueAsString(new ResponseDto(true, "Device successfully created", null, null)));
+            } catch (SQLException e) {
+                log.error(e.toString());
+                ctx.getResponse().status(500).send(new ObjectMapper().writeValueAsString(new ResponseDto(false, "Failed to create device", null, null)));
+            }
         });
     }
 }
--- src/main/java/org/example/services/DeviceService.java
+++ src/main/java/org/example/services/DeviceService.java
@@ -4,10 +4,8 @@ import org.example.config.DbConfig;
 import org.example.dtos.CreateDeviceDto;
 
 import javax.inject.Inject;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDateTime;
 
 public class DeviceService {
     private final DbConfig dbConfig;
@@ -18,15 +16,48 @@ public class DeviceService {
     }
 
     public void createDevice(CreateDeviceDto device) throws SQLException {
-        String script = "INSERT INTO devices (brand_name, device_name, device_description, min_value, max_value, default_value) VALUES (?, ?, ?, ?, ?, ?)";
-        try(Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(script)){
+        try (Connection connection = getConnection()) {
+            connection.setAutoCommit(false);
+            try {
+                Integer deviceId = insertDevice(connection, device);
+                for(String country : device.targetCountry()){
+                    insertTargetCountry(connection, deviceId, country);
+                }
+                connection.commit();
+            } catch (Exception e) {
+                connection.rollback();
+                throw e;
+            }
+        }
+    }
+
+    private void insertTargetCountry(Connection connection, Integer deviceId, String country) throws SQLException {
+        String script = "INSERT INTO device_target_countries (country_code, device_id) VALUES (?, ?)";
+        try(PreparedStatement statement = connection.prepareStatement(script)){
+            statement.setString(1, country);
+            statement.setInt(2, deviceId);
+            statement.executeUpdate();
+        }
+    }
+
+    private Integer insertDevice(Connection connection, CreateDeviceDto device) throws SQLException {
+        String script = "INSERT INTO devices (brand_name, device_name, device_description, min_value, max_value, default_value, created_at) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(script, Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, device.brandName());
             statement.setString(2, device.deviceName());
             statement.setString(3, device.deviceDescription());
             statement.setInt(4, device.deviceConfiguration().minValue());
             statement.setInt(5, device.deviceConfiguration().maxValue());
             statement.setInt(6, device.deviceConfiguration().defaultValue());
+            statement.setTimestamp(7, Timestamp.valueOf(LocalDateTime.now()));
             statement.executeUpdate();
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                if(resultSet.next()){
+                    return resultSet.getInt(1);
+                } else {
+                    throw new SQLException("Failed to retrieve device Id");
+                }
+            }
         }
     }
 
