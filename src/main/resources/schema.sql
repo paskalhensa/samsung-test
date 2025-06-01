20f3dfc64c036cfaa635c65d853813ae710cf853
paskalhensa <paskalis_henry@yahoo.com>
2025-06-01 12:40:04 +0700

    feat: adjust schema.sql for user login

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,27 +11,41 @@ CREATE TABLE devices (
     deleted_at DATETIME
 );
 
+CREATE TABLE countries (
+    code CHAR(2) PRIMARY KEY,
+    country_name VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE users (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(100) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    created_at DATETIME DEFAULT SYSDATETIME()
+);
+
+GO
+
+CREATE TABLE smartthings_user_profiles (
+    user_id INT PRIMARY KEY
+    full_name VARCHAR(100),
+    dob DATE,
+    user_address TEXT,
+    country_code CHAR(2),
+    FOREIGN KEY (country_code) REFERENCES countries(code),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+GO
+
 CREATE TABLE user_devices (
     id INT IDENTITY(1,1) PRIMARY KEY,
     current_value INT,
     user_id INT,
     device_id INT,
-    FOREIGN KEY (user_id) REFERENCES smartthings_users(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (device_id) REFERENCES devices(id)
-);
-
-CREATE TABLE smartthings_users (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    user_name VARCHAR(100),
-    dob DATE,
-    address TEXT,
-    country_code CHAR(2),
-    FOREIGN KEY (country_code) REFERENCES countries(code)
-);
-
-CREATE TABLE countries (
-    code CHAR(2) PRIMARY KEY,
-    country_name
 );
 
 CREATE TABLE device_target_countries (
